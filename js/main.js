0390bf12d6c169eb34691d1771278f8c862e6455
Clay Wright <gclaywright@gmail.com>
2024-05-23 18:00:38 -0700

    Created a starting task.html and a Task class

---
+++ js/main.js
@@ -0,0 +1,6 @@
+class Task {
+}
+let myTask = new Task();
+myTask.taskname = "Shop for groceries";
+myTask.status = true;
+console.log(myTask);
---
+++ ts/main.ts
@@ -0,0 +1,21 @@
+/**
+ * Represents a single task that can be added.
+ */
+class Task {
+    /**
+     * The name of the task
+     */
+    taskname : string;
+
+    /**
+     * Checks if the task is complete
+     */
+    status : boolean;
+}
+
+// Task object test code
+let myTask = new Task();
+myTask.taskname = "Shop for groceries";
+myTask.status = true;
+
+console.log(myTask);
